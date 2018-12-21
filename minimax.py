6d7ecd80dcc85e683aa2533d0c82359955ad5a2d
LatvianProgrammer <kalvans.rolands@gmail.com>
2018-12-21 21:36:45 +0200

    simplified functions

--- minimax.py
+++ minimax.py
@@ -10,18 +10,11 @@ class TicTacToe:
         self.current_game_state = 0
         self.transposition_table = {}
         self.current_player_move = self.player_x
-        self.mini_max(0, 0, 1)
+        self.mini_max(self.current_game_state, 0, self.current_player_move)
 
     @staticmethod
     def win(game_state):
-        return ((game_state & 0o007) == 0o007 or
-                (game_state & 0o070) == 0o070 or
-                (game_state & 0o700) == 0o700 or
-                (game_state & 0o111) == 0o111 or
-                (game_state & 0o222) == 0o222 or
-                (game_state & 0o444) == 0o444 or
-                (game_state & 0o421) == 0o421 or
-                (game_state & 0o124) == 0o124)
+        return any((game_state & mask) == mask for mask in [0o007, 0o070, 0o700, 0o111, 0o222, 0o444, 0o421, 0o124])
 
     def win_o(self, game_state):
         return self.win(game_state)
@@ -41,63 +34,53 @@ class TicTacToe:
             return depth - 10
         elif self.win_x(game_state):
             return 10 - depth
-        else:
-            return 0
+        return 0
 
     @staticmethod
     def do_ply(game_state, ply):
         return game_state | ply
 
     @staticmethod
-    def generate_plies(game_state, plies, player):
-        for i in range(9):
-            if not (((game_state >> 9) | game_state) & (1 << i)):
-                plies.append(1 << (i + player * 9))
+    def generate_plies(game_state, player):
+        plies = [1 << (i + player * 9) for i in range(9) if not (((game_state >> 9) | game_state) & (1 << i))]
+        random.shuffle(plies)
+        return plies
 
-    def mini_max(self, game_state, depth, maximising_player):
-        possible_plies = []
+    def mini_max(self, game_state, depth, player):
 
         best_ply = None
 
         if self.end_state(game_state):
             return None, self.evaluation(game_state, depth)
 
-        if maximising_player:  # X is maximising player
+        if player == self.player_x:  # X is maximising player
             best_value = -sys.maxsize - 1
-            self.generate_plies(game_state, possible_plies, self.player_x)
 
-            random.shuffle(possible_plies)
-
-            for i in possible_plies:
-                next_game_state = self.do_ply(game_state, i)
+            for ply in self.generate_plies(game_state, self.player_x):
+                next_game_state = self.do_ply(game_state, ply)
                 if next_game_state not in self.transposition_table:
-                    next_ply, next_value = self.mini_max(next_game_state, depth + 1, not maximising_player)
+                    next_ply, next_value = self.mini_max(next_game_state, depth + 1, self.player_o)
                     self.transposition_table[next_game_state] = next_value
-
                 else:
                     next_value = self.transposition_table[next_game_state]
 
                 if next_value > best_value:
                     best_value = next_value
-                    best_ply = i
+                    best_ply = ply
         else:
             best_value = sys.maxsize
-            self.generate_plies(game_state, possible_plies, self.player_o)
 
-            random.shuffle(possible_plies)
-
-            for i in possible_plies:
-                next_game_state = self.do_ply(game_state, i)
+            for ply in self.generate_plies(game_state, self.player_o):
+                next_game_state = self.do_ply(game_state, ply)
                 if next_game_state not in self.transposition_table:
-                    next_ply, next_value = self.mini_max(next_game_state, depth + 1, not maximising_player)
+                    next_ply, next_value = self.mini_max(next_game_state, depth + 1, self.player_x)
                     self.transposition_table[next_game_state] = next_value
-
                 else:
                     next_value = self.transposition_table[next_game_state]
 
                 if next_value < best_value:
                     best_value = next_value
-                    best_ply = i
+                    best_ply = ply
 
         return best_ply, best_value
 
@@ -114,7 +97,7 @@ class TicTacToe:
         print()
 
     def next_move(self):
-        if self.current_player_move == 1:
+        if self.current_player_move == self.player_x:
             ply = self.mini_max(self.current_game_state, 0, self.current_player_move)[0]
         else:
             ply = 1 << (int(input('enter move 0-8: ')) + self.current_player_move * 9)
@@ -125,7 +108,6 @@ class TicTacToe:
     def reset(self):
         self.current_game_state = 0
         self.current_player_move = self.player_x
-
 
 
 if __name__ == '__main__':
